e8e3c1cd53908acf396ff58227693208efa5adb5
HugoRCD <hugo.richard@epitech.eu>
2023-03-02 19:49:32 +0100

    Add footer and update command console

---
+++ components/layout/Footer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <footer class="bg-primary">
+    <div class="mx-auto max-w-7xl overflow-hidden py-10 px-6 lg:px-8">
+      <hr class="mt-10 border-t-2 border-muted rounded-lg" />
+      <div class="mt-10 flex flex-col justify-center items-center space-y-4">
+        <nuxt-img class="w-auto h-6" :src="'/supabase/assets/maison-hochard-logo'" />
+        <span class="text-muted text-sm"
+        >© 2023
+          <NuxtLink
+            target="_blank"
+            to="https://www.maisonhochard.com"
+            class="text-muted hover:text-primary transition-colors duration-300 ease-in-out"
+          >
+            Maison Hochard </NuxtLink
+          >. All rights reserved.</span
+        >
+      </div>
+    </div>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import CommandConsole from "~/components/layout/CommandConsole.vue";
 import Navbar from "~/components/layout/Navbar.vue";
+import Footer from "~/components/layout/Footer.vue";
 </script>
 
 <template>
@@ -8,6 +9,7 @@ import Navbar from "~/components/layout/Navbar.vue";
     <CommandConsole />
     <Navbar />
     <slot class="h-full" />
+    <Footer />
   </div>
 </template>
 
