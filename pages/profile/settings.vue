fc4e8cb6132a9e265ce65916138da1db7b444da7
HugoRCD <hugo.richard@epitech.eu>
2023-03-03 18:36:41 +0100

    Settings page done

--- pages/profile/settings.vue
+++ pages/profile/settings.vue
@@ -9,16 +9,15 @@ definePageMeta({
 </script>
 
 <template>
-  <div>
-    <div class="card">
-      <h3 class="text-lg font-medium leading-6 text-primary">Settings</h3>
+    <div class="bg-secondary px-4 py-5 shadow sm:p-6">
+      <h3 class="text-lg font-medium leading-6 text-primary">{{ $t("settings.title") }}</h3>
       <div class="mt-2 max-w-xl text-sm text-muted">
-        <p>Here you can change the application settings.</p>
+        <p>{{ $t("settings.description") }}</p>
       </div>
-      <div class="mt-5 flex gap-4">
+      <div class="mt-5 flex gap-4 items-center">
+        <span class="text-muted">{{ $t("settings.language") }}:</span>
         <LanguageSelector :isText="true" />
       </div>
     </div>
-  </div>
 </template>
 
